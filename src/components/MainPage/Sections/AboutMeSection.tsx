7bc9c628e9c0c37cc5a9e4e46d7241371baee5e8
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-02-03 23:43:51 +0800

    quote remove

--- src/components/MainPage/Sections/AboutMeSection.tsx
+++ src/components/MainPage/Sections/AboutMeSection.tsx
@@ -12,7 +12,7 @@ export const AboutMeSection: React.FC = () => {
                 <div className="flex flex-col gap-5">
                     <h1 className="text-xl lg:text-4xl font-bold">🙋‍♂️ About Me</h1>
                     <p className="leading-loose text-sm lg:text-md text-[#54555F]">
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                     </p>
                 </div>
                 <CustomCarousel>
